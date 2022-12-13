1063284435814cbf63ad5dde8b8bfb0261d27d37
Lukanko Dagmara <dagmaraluk@purpleocean.net>
2022-12-13 17:50:12 +0900

    fix: launchesModel update

--- server/src/models/launchesModel.js
+++ server/src/models/launchesModel.js
@@ -34,6 +34,8 @@ function addNewLaunch(launch) {
 
 function abortMissionById(launchId) {
   const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
   return aborted;
 }
 
